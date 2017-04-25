42dc4562e0389bd6b7938308534bd3b0aa8d38da
circleci <circleci@mail.com>
2017-04-25 14:28:12 +0000

    [ci skip] Update site

--- dist/hawtio-forms-test.js
+++ dist/hawtio-forms-test.js
@@ -3727,7 +3727,7 @@ var Forms2Tests;
             }, 1000));
         }]);
     Forms2Tests._module.controller("HawtioFormsTests.Forms2WizardController", ["$scope", "$templateCache", function ($scope, $templateCache) {
-            var config = _.clone(baseConfig, true);
+            var config = _.cloneDeep(baseConfig);
             config.wizard = {
                 onChange: function (current, next, pageIds) {
                     log.debug("page changed, current page: ", current, " next: ", next);
@@ -3759,7 +3759,7 @@ var Forms2Tests;
                 }
             };
             $scope.config = config;
-            var model = _.clone(baseModel, true);
+            var model = _.cloneDeep(baseModel);
             $scope.model = model;
             $scope.configStr = angular.toJson($scope.config, true);
             $scope.markup = $templateCache.get("markup.html");
@@ -4016,13 +4016,13 @@ var Forms2Tests;
             }, 1000));
         }]);
     Forms2Tests._module.controller("HawtioFormsTests.Forms2TabsController", ["$scope", "$templateCache", function ($scope, $templateCache) {
-            var config = _.clone(baseConfig, true);
+            var config = _.cloneDeep(baseConfig);
             config.tabs = {
                 "Tab One": ["scheme", "array3", "key", "value"],
                 "Tab Two": ["*"],
                 "Tab Three": ["booleanArg"]
             };
-            var model = _.clone(baseModel, true);
+            var model = _.cloneDeep(baseModel);
             $scope.config = config;
             $scope.model = model;
             $scope.configStr = angular.toJson($scope.config, true);
@@ -4042,10 +4042,10 @@ var Forms2Tests;
             }, 1000));
         }]);
     Forms2Tests._module.controller("HawtioFormsTests.Forms2Controller", ["$scope", "$templateCache", "SchemaRegistry", function ($scope, $templateCache, schemas) {
-            var config = _.clone(baseConfig, true);
+            var config = _.cloneDeep(baseConfig);
             config.controls = ["scheme", "nestedObject", "fromSchemaRegistry", "*", "array2", "array1"];
             $scope.config = config;
-            var model = _.clone(baseModel, true);
+            var model = _.cloneDeep(baseModel);
             $scope.model = model;
             $scope.configStr = angular.toJson($scope.config, true);
             $scope.markup = $templateCache.get("markup.html");
